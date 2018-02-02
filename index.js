696dc7934104bd66ca7ccdb9e7e3627c1fa9b724
JohnCoder <johnsmithcoder7@gmail.com>
2018-02-02 18:03:20 -0500

    fix and update in preperation for v2

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
-const Sandysounds = require('./src/SandySounds');
+const V1 = require('./src/versions/v1/SandySounds');
+const V2 = require('./src/versions/v2/SandySounds');
 const DefaultClient = require('./src/clients/Default');
 
 function SandySounds(...args) {
-    return new Sandysounds(...args);
+    return new V1(...args);
 }
 
 SandySounds.Clients = {
     Default: DefaultClient
+};
+
+SandySounds.Versions = {
+    V1: V1,
+    V2: V2
 };
 
 module.exports = SandySounds;
